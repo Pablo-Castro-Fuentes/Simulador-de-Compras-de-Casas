a0439a6173535f4f2794da82b7582ef049895755
Daniel Torres <danielte98@gmail.com>
2025-03-28 23:08:08 -0300

    creacion index

--- core/settings.py
+++ core/settings.py
@@ -125,8 +125,11 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / "apps/common/static",
     "static/",
 ]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
